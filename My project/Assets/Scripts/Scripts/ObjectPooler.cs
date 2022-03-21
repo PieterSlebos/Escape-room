0132f8e8737ffbdc16c8e334ea5946271e4d1326
PieterSlebos <pieter.slebos@student.hu.nl>
2022-03-21 09:34:22 +0100

    mijn level
    mijn level

--- My project/Assets/Scripts/Scripts/ObjectPooler.cs
+++ My project/Assets/Scripts/Scripts/ObjectPooler.cs
@@ -12,6 +12,17 @@ public class ObjectPooler : MonoBehaviour
         public GameObject prefab;
 
     }
+    #region Singleton
+
+
+    public static ObjectPooler Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+
 
     public List<Pool> pools;
     public Dictionary<string, Queue<GameObject>> poolDictionary;
@@ -26,19 +37,38 @@ public class ObjectPooler : MonoBehaviour
 
             for (int i = 0; i < pool.size; i++)
             {
-               GameObject obj = Instantiate(pool.prefab);
+                GameObject obj = Instantiate(pool.prefab);
                 obj.SetActive(false);
                 objectPool.Enqueue(obj);
 
             }
             poolDictionary.Add(pool.tag, objectPool);
         }
-        
+
     }
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag " + tag + " doesn't excist.");
+            return null;
+        }
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 }
