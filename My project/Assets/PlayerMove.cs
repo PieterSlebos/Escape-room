f881f50adec3ce18458efbdd8dd36699ee1e3b44
PieterSlebos <pieter.slebos@student.hu.nl>
2022-04-05 10:22:33 +0200

    first person controler

--- My project/Assets/PlayerMove.cs
+++ My project/Assets/PlayerMove.cs
@@ -2,23 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
-
-    public CharacterController controller;
+    public CharacterController controller; 
 
     public float speed = 12f;
     public float gravity = -9.81f;
-
-    Vector3 velocity;
-
-    public float jumpHeight = 3f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
-    
+    Vector3 velocity;
     bool isGrounded;
 
     // Update is called once per frame
@@ -31,21 +26,16 @@ public class Player : MonoBehaviour
             velocity.y = -2f;
         }
 
-        // Moving
-
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
-
+        
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
 
-        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-
-        
     }
 }
