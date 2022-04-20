f32fedceb965c823a62822fd8a5e9c1ad537d348
Tobias-vs <90895120+Tobias-vs@users.noreply.github.com>
2022-04-20 14:00:42 +0200

    New portal + random scene portal script

--- My project/Assets/Scripts/ScriptsTobias/LoadScenePortal.cs
+++ My project/Assets/Scripts/ScriptsTobias/LoadScenePortal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -7,13 +8,64 @@ using UnityEngine.SceneManagement;
 public class LoadScenePortal : MonoBehaviour
 {
 
-    public string NextScene = "";
+    public string nextScene = "";
+
+    public List<string> ListOfScenes = new List<string>();
+
+    public int randomSceneNumber;
+    public string randomSceneName;
+
+    private void Start()
+    {
+        FillSceneList();
+
+        RandomSceneNum();
+
+        GetRandomSceneName();
+
+    }
+    
+    private void FillSceneList()
+    {
+        ListOfScenes.Add("Level-Moos");
+        ListOfScenes.Add("Level_balls");
+        ListOfScenes.Add("Scene_Level_Tobias");
+    }
+
+    private void RandomSceneNum()
+    {
+        int listCount = ListOfScenes.Count;
+        listCount++;
+
+        int num = Random.Range(0, listCount);
+        randomSceneNumber = num;
+    }
+
+    private void GetRandomSceneName()
+    {
+        string sceneName;
+        sceneName = ListOfScenes[randomSceneNumber].ToString();
+        randomSceneName = sceneName;
+    }
+
+    private void LoadRandomScene()
+    {
+        SceneManager.LoadScene(randomSceneName);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            SceneManager.LoadScene(NextScene);
+            if(nextScene == "Random")
+            {
+                LoadRandomScene();
+                SceneManager.LoadScene(randomSceneName);
+            } 
+            else
+            {
+                SceneManager.LoadScene(nextScene);
+            }
         }
     }
 
