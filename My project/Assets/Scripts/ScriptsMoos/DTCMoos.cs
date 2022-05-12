5ca571bda2d301379202cadeb2f2e32de31fb5f9
moosmostert <moos.mostert@student.hu.nl>
2022-05-12 22:13:35 +0200

    HUUUUGE POGCHAMP LETSGOOO
    -added door animation
    -added door trigger with button
    -button dissapears when walked over need to fix somehow idk
    -but when button dissapears door opens animated its awesome
    -made some parkour platforms to get to the button
    -Idk how the cube is gonna be apart of this since it doesn't do shit when it touches the button I god please help

---
+++ My project/Assets/Scripts/ScriptsMoos/DTCMoos.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DTCMoos : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor = null;
+
+    [SerializeField] private bool openTrigger = false;
+    [SerializeField] private bool closedTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (openTrigger)
+            {
+                myDoor.Play("Door_open", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+
+            else if (closedTrigger)
+            {
+                myDoor.Play("Door_close", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
+
