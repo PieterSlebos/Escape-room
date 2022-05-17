01ba64ed1220edc95a912d997aa6cd70d75c7eca
moosmostert <moos.mostert@student.hu.nl>
2022-05-17 10:24:24 +0200

    fixing bitch

--- My project/Assets/Scripts/ScriptsMoos/DTCMoos.cs
+++ My project/Assets/Scripts/ScriptsMoos/DTCMoos.cs
@@ -11,7 +11,7 @@ public class DTCMoos : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") || other.CompareTag("Cube"))
         {
             if (openTrigger)
             {
